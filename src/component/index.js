fac6d2b058560ee4c7a4cb581c21f8de9b269155
chendapeng <291003932@qq.com>
2021-01-02 10:26:24 +0800

    新功能(promise手写): 手写promise

--- src/component/index.js
+++ src/component/index.js
@@ -2,7 +2,7 @@ import TheLoading from './TheLoading.vue'
 // import Vue from 'vue'
 export default {
   install(Vue, options) {
-    console.log('options', options);
+    // console.log('options', options);
     console.log('TheLoading', TheLoading);
     Vue.component(TheLoading.name, TheLoading)
   }
