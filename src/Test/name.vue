04f248ba14254f5df94d2354351f55cc9be9f886
niaogege <niaogege66@163.com>
2021-12-30 18:24:31 +0800

    feat: 测试

--- src/Test/name.vue
+++ src/Test/name.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-11-15 19:47:42
- * @LastEditTime: 2021-12-30 18:11:26
+ * @LastEditTime: 2021-12-30 18:23:42
  * @LastEditors: Chendapeng
  * @Description: In User Settings Edit
  * @FilePath: \plugin-cppd:\learn\webpack-learn\src\Test\name.vue
@@ -15,7 +15,7 @@
 export default {
   props: ['name'],
   mounted() {
-    console.log('111');
+    // console.log('111');
   }
 }
 </script>
